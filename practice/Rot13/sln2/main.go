f404732414425bea44217c04525c37c1b77223d4
garveyshah <garveyshah1595@gmail.com>
2024-05-05 04:10:50 +0300

    rot 13 solutions

--- practice/Rot13/sln2/main.go
+++ practice/Rot13/sln2/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-
+	"fmt"
 	"github.com/01-edu/z01"
 	"os"
 )
@@ -10,20 +10,25 @@ func main() {
 	if len(os.Args) < 2 {
 		return
 	}
-	args := os.Args[0]
+	args := os.Args[1]
 
-	var result []byte
+	fmt.Println(args)
+	result := []byte{}
+
 	alphLib := map[byte]byte{'A': 'N', 'B': 'O', 'C': 'P', 'D': 'Q', 'E': 'R', 'F': 'S', 'G': 'T', 'H': 'U', 'I': 'V', 'J': 'W', 'K': 'X', 'L': 'Y', 'M': 'Z', 'N': 'A', 'O': 'B', 'P': 'C', 'Q': 'D', 'R': 'E', 'S': 'F', 'T': 'G', 'U': 'H', 'V': 'I', 'W': 'J', 'X': 'K', 'Y': 'L', 'Z': 'M', 'a': 'n', 'b': 'o', 'c': 'p', 'd': 'q', 'e': 'r', 'f': 's', 'g': 't', 'h': 'u', 'i': 'v', 'j': 'w', 'k': 'x', 'l': 'y', 'm': 'z', 'n': 'a', 'o': 'b', 'p': 'c', 'q': 'd', 'r': 'e', 's': 'f', 't': 'g', 'u': 'h', 'v': 'i', 'w': 'j', 'x': 'k', 'y': 'l', 'z': 'm'}
 
 	for i := 0; i < len(args); i++ {
-		if val, ok := alphLib[args[i]]; ok {
+		char1 := args[i]
+		if val, ok := alphLib[char1]; ok {
 			result = append(result, val)
 		} else {
-			result = append(result, args[i])
+			result = append(result, char1)
 		}
 	}
-	for _,char := range result {
+	Result2 := string(result)
+	
+	for _, char := range Result2 {
 		z01.PrintRune(rune(char))
-		z01.PrintRune('\n')
 	}
+	z01.PrintRune('\n')
 }
