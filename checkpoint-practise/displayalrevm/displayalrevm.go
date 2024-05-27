479867a0fa5eef32faddffcaea44d4696502b2f4
garveyshah <garveyshah1595@gmail.com>
2024-05-27 20:35:58 +0300

    new practise question

---
+++ checkpoint-practise/displayalrevm/displayalrevm.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+	str := "abcdefghijklmnopqrstuvwxyz"
+	for i := len(str) - 1; i >= 0; i-- {
+		char := str[i]
+		if i%2 == 0 {
+			z01.PrintRune(rune(char - 32))
+		}
+		z01.PrintRune(rune(char))
+	}
+	z01.PrintRune('\n')
+}
