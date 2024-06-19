b1d1fa81e6a5eebc3155f092a47b6bb5c5fe3e94
garveyshah <garveyshah1595@gmail.com>
2024-06-19 18:54:54 +0300

    updated the solution for the romannumbers program

--- checkpoint-practise/romannumbers/rn.go
+++ checkpoint-practise/romannumbers/rn.go
@@ -28,15 +28,24 @@ func main() {
 		return
 	}
 
- RomanNums := map[int]string{
-        1:  "I", 2:  "II", 3:  "III", 4:  "IV", 5:  "V",
-        6:  "VI", 7:  "VII", 8:  "VIII", 9:  "IX", 10: "X",
-        11: "XI", 12: "XII", 13: "XIII", 14: "XIV", 15: "XV",
-        16: "XVI", 17: "XVII", 18: "XVIII", 19: "XIX", 20: "XX",
-        21: "XXI", 22: "XXII", 23: "XXIII", 24: "XXIV", 25: "XXV",
-        26: "XXVI", 27: "XXVII", 28: "XXVIII", 29: "XXIX", 30: "XXX",
-		50: "L", 100: "C", 500: "D", 1000: "M",
-    }
-	
-	
+	RomanNums := map[int]string{
+		1: "I", 4: "IV", 5: "V",
+		9: "IX", 10: "X", 40: "XL",
+		50: "L", 90: "XC", 100: "C",
+		400: "CD", 500: "D", 900: "CM",
+		1000: "M",
+	}
+
+	// Slice to maintain the order of the Roman numeral values
+	values := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+
+	final := ""
+
+	for _, value := range values {
+		for result >= value {
+			final += RomanNums[value]
+			result -= value
+		}
+	}
+	fmt.Printf("%s\n", final)
 }
