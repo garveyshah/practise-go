c3ec3e9030e188e61a6cb85a47a4e8918bb3a9f3
oumouma <garveyshah1595@gmail.com>
2024-07-01 21:07:29 +0300

    untracked files

--- checkpoint-practise/doop/doop.go
+++ checkpoint-practise/doop/doop.go
@@ -1,22 +1,75 @@
 package main
 
 import (
+	"fmt"
 	"os"
+)
 
-	if len(os.Args) != 3 {
+func main() {
+	if len(os.Args) != 4 {
 		return
 	}
 
 	Value1 := os.Args[1]
-	operator := rune(os.Args[2])
+	operator := (os.Args[2])
 	Value2 := os.Args[3]
 	result := 0
-	if operator == '-' {
-		result = Value1 - Value2
-	}
-	if result == '+' {
-		result = Value1 + Value
+
+	num1, err := StringToInt(Value1)
+	if err != nil {
+		return
 	}
 
+	num2, err := StringToInt(Value2)
+	if err != nil {
+		return
+	}
+	if (num1 >= 9223372036854775807) || (num2 >= 9223372036854775807) || (num1 <= int(-9223372036854775807)) || (num2 <= -9223372036854775807) {
+		return
+	}
+	if operator == "-" {
+		result = num1 - num2
+	} else if operator == "+" {
+		result = num1 + num2
+	} else if operator == "/" {
+		if num1 == 0 || num2 == 0 {
+			fmt.Println("No division by 0")
+			return
+		} else {
+			result = num1 / num2
+		}
+	} else if operator == "%" {
+		if num1 == 0 || num2 == 0 {
+			fmt.Println("No modulo by 0")
+			return
+		} else {
+			result = num1 % num2
+		}
+	}  else if operator == "*" {
+		result = num1 * num2
+		//fmt.Println(result)
+	}else {
+		return
+	}
+	fmt.Println(result)
+}
 
-)
+func StringToInt(s string) (int, error) {
+	var result int
+	var err error
+	sigh := 1
+	if s[0] == '-' {
+		s = s[1:]
+		sigh = -1
+	}
+	for _, char := range s {
+		if char < '0' || char > '9' {
+			return 0, err
+		}
+
+		result = result*10 + int(char-'0')
+	}
+
+	result = sigh * result
+	return result, err
+}
