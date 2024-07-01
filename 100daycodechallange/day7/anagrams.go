e7859a10a49f4535be5d364e895693c01022bc6d
oumouma <garveyshah1595@gmail.com>
2024-07-01 21:32:08 +0300

    complition of impletion of a program and function to confirm if 2 strings are anagrams

--- 100daycodechallange/day7/anagrams.go
+++ 100daycodechallange/day7/anagrams.go
@@ -26,13 +26,20 @@ func main() {
 
 	MainStr = strings.TrimSpace(MainStr)
 
-	Str1, Str2 :=GetInput(MainStr)
-	fmt.Printf("String 1 is :%v.\nString 2 is :%v.",Str1, Str2)
+	str1, str2 :=GetInput(MainStr)
+	//fmt.Printf("String 1 is :%v.\nString 2 is :%v.",Str1, Str2)
+
+	if ConfirmAnagrams(str1, str2) {
+		fmt.Printf("\n%v and %v are anagrams.\n", str1, str2)
+	} else {
+		fmt.Printf("\n%v and %v are NOT anagrams of each other.\n", str1, str2)
+	}
 
 
 
 }
 
+// Function to format the input into two strings.
 func GetInput(s string) (string, string){
 	strSlice := strings.Split(s, " ")
 	
@@ -40,4 +47,33 @@ func GetInput(s string) (string, string){
 	str2 := strings.Trim(strSlice[1],"")
 
 	return str1, str2
+}
+
+// Function that checks if two strings are anagrams.
+func ConfirmAnagrams(str1, str2 string) bool{
+	if len(str1) != len(str2) {
+		return false
+	}
+
+	countChar := make(map[rune]int)
+
+	for _, char  :=range str1 {
+		countChar[char]++
+	}
+
+	// Decrease count for each character in str2
+	for _, char := range str2 {
+		if countChar[char] == 0 {
+			return false
+		}
+		countChar[char]--
+	}
+
+	// Check if all count are zero
+	for _, count := range countChar {
+		if count != 0 {
+			return false
+		}
+	}
+	return true
 }
