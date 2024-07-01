a84f1e4c99f4fd0fb84735eaa43f3522b5c036e2
oumouma <garveyshah1595@gmail.com>
2024-07-01 20:47:29 +0300

    implemnet a program and function to check if two strings are anagrams

--- 100daycodechallange/day7/anagrams.go
+++ 100daycodechallange/day7/anagrams.go
@@ -2,9 +2,8 @@
 Day 7: Implement a function to check if two strings are anagrams.
 */
 
-
-//workflow
-//1. interate through the strings look for instances of "" if found append to separate string.
+// workflow
+// 1. interate through the strings look for instances of "" if found append to separate string.
 package main
 
 import (
@@ -25,19 +24,11 @@ func main() {
 	}
 
 	MainStr = strings.TrimSpace(MainStr)
-
-	Str1, Str2 :=GetInput(MainStr)
-	fmt.Printf("String 1 is :%v.\nString 2 is :%v.",Str1, Str2)
-
-
+	fmt.Println(MainStr)
 
 }
 
-func GetInput(s string) (string, string){
-	strSlice := strings.Split(s, " ")
+func GetInput(s string) string{
 	
-	str1 := strings.Trim(strSlice[0],"")
-	str2 := strings.Trim(strSlice[1],"")
-
-	return str1, str2
+	return ""
 }
