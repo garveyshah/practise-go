cb830d30b5a6443e793a8793a32351b296aecb84
oumouma <garveyshah1595@gmail.com>
2024-07-02 06:33:23 +0300

    stat of day9 program

---
+++ 100daycodechallange/day9/factorial.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+)
+
+func main() {
+
+	fmt.Println("Enter the number... Then press enter.")
+
+	reader := 
+
+}
+
+func Factorial(num int) int {
+	return int
+}
+
+// Fuction to convert string digits to intergers.
+func CustomAtoi(s strings) (int, error){
+	return 0, nil
+}
