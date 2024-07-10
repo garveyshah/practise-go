f3490e28b5c564bcafafe13f4a0f3e2e84b54a43
oumouma <garveyshah1595@gmail.com>
2024-07-11 01:14:07 +0300

    rot13 implementation

---
+++ checkpoint-practise/rot13/rot13.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		return
+	}
+
+	plainText := os.Args[1]
+	cypherText := ""
+	for _, char := range plainText {
+		switch {
+		case char >= 'A' && char <= 'Z':
+			cypherText += string((char+13-'A')%26 + 'A')
+		case char >= 'a' && char <= 'z':
+			cypherText += string((char+13 -'a')%26 + 'a')
+		default:
+			cypherText += string(char)
+		}
+	}
+	for _,char := range cypherText {
+		z01.PrintRune(char)
+	}
+	z01.PrintRune(10)
+}
