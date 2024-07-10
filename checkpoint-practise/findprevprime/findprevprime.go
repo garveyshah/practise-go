e55f6788dea3326840260061ad0287185616eeb7
oumouma <garveyshah1595@gmail.com>
2024-07-10 09:54:24 +0300

    a program that finds the first prime number that is equal or inferior to the int passed as parameter.

---
+++ checkpoint-practise/findprevprime/findprevprime.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		return
+	}
+
+	num, _ := customeAtoi(os.Args[1])
+
+	fmt.Println(FindNextPrime(num))
+}
+
+func FindNextPrime(num int) int {
+	result := []int{}
+	for i := 0; i <= num; i++ {
+		if IsPrime(i) {
+			result = append(result, i)
+		}
+	}
+	if len(result) == 0 {
+		return 0
+	}
+	return result[len(result)-1]
+}
+
+func IsPrime(num int) bool {
+	result := []int{}
+	for i := 1; i <= num; i++ {
+		if num%i == 0 {
+			result = append(result, i)
+		}
+	}
+	return len(result) == 2
+}
+
+func customeAtoi(s string) (int, error) {
+	result := 0
+	sign := 1
+	if s[0] == '-' {
+		s = s[1:]
+		sign = -1
+	}
+
+	for _, char := range s {
+		if char < '0' || char > '9' {
+			return 0, fmt.Errorf("invalid char")
+		}
+		result = result*10 + int(char-'0')
+	}
+	result = sign * result
+	return result, nil
+}
