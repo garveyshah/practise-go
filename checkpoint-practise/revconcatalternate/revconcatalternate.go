da1593f4d2b06cdd60ee9bb4301af4c7254a55b1
oumouma <garveyshah1595@gmail.com>
2024-07-28 02:44:27 +0300

    reverse contatinate

---
+++ checkpoint-practise/revconcatalternate/revconcatalternate.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.RevConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6}))
+	fmt.Println(piscine.RevConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6, 7, 8, 9}))
+	fmt.Println(piscine.RevConcatAlternate([]int{1, 2, 3, 9, 8}, []int{4, 5}))
+	fmt.Println(piscine.RevConcatAlternate([]int{1, 2, 3}, []int{}))
+}
+
+func RevConcatAlternate(slice1,slice2 []int) []int {
+
+	for slice1,slice2
+
+}
