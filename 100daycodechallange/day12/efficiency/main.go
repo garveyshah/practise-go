afcfdfa63f72f256142ee253d7697a1c8fea9acc
oumouma <garveyshah1595@gmail.com>
2024-08-05 06:18:21 +0300

    skeleton of the implementation

---
+++ 100daycodechallange/day12/efficiency/main.go
@@ -0,0 +1,7 @@
+/*
+### 3. Efficiency
+Write a Go program to remove duplicates from a large array (e.g., 10,000 elements) and evaluate its time complexity.
+Describe any trade-offs between time complexity and space complexity in your solution.
+*/
+
+package main
