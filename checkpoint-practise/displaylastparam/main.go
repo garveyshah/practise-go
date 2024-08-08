acce395e71041a222e174c4b338f621f84a0866a
oumouma <garveyshah1595@gmail.com>
2024-08-09 00:25:18 +0300

    implementation of lastparam

---
+++ checkpoint-practise/displaylastparam/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		return
+	}
+
+	for _, char := range os.Args[len(os.Args)-1] {
+		z01.PrintRune(char)
+	}
+	z01.PrintRune(10)
+}
