37a051ebeb81c1110365fbf5226e623cf7fe6aee
oumouma <garveyshah1595@gmail.com>
2024-08-09 06:01:21 +0300

    implementation of roman numbers

--- checkpoint-practise/romannumbers/rn.go
+++ checkpoint-practise/romannumbers/rn.go
@@ -3,31 +3,53 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"project01/checkpoint-practise/atoi/atoi"
 )
 
- // Mpa of integer values to their corresponding Roman numeral symbols
-var romanNumerals = []struct {
-	Value int
-	Symbol string
-}{
-	{1000, "M"},
-	{900,"CM"},
-	{500,"D"},
-	{400,"CD"},
-	{100,"C"},
-	{90,"XC"},
-	{50,"L"},
-	{40,"XL"},
-	{10,"X"},
-	{9,"IX"},
-	{5,"V"},
-	{4,"IV"},
-	{1,"I"},
+// Convert an integer to a ROman numeral with step tracking
+func IntToroman(num int) (string, string, error) {
+	if num <= 0 || num >= 4000 {
+		return "", "", fmt.Errorf("cannot convert to roman digit")
+	}
+
+	symbols := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+	calculation := []string{"M", "M-C", "D", "D-C", "C", "C-X", "L", "L-X", "X", "X-I", "V", "V-I", "I"}
+	number := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+	
+	roman := ""
+	calc := ""
+
+	for i := 0; i < len(number); i++ {
+		for num >= number[i] {
+			roman += symbols[i]
+			if len(calculation[i]) > 1 {
+				calc += "(" + calculation[i] + ")" + "+"
+			} else {
+				calc += calculation[i] + "+"
+			}
+			num -= number[i]
+			// n = 2000 - 1000
+		}
+	}
+
+	if calc[len(calc)-1] == '+' {
+		calc = calc[:len(calc)-1]
+	}
+	return roman, calc, nil
 }
 
-// Convert an integer to a ROman numeral with step tracking
-func intToroman(num int) (string, string, error) {
-	if num <= 0 || num >= 4000 {
-		return "", "", fmt.
+func main() {
+	if len(os.Args) < 2 {
+		return
 	}
+
+	roman, steps, err := IntToroman(atoi.Atoi(os.Args[1]))
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	fmt.Println(roman)
+	fmt.Println(steps)
 }
