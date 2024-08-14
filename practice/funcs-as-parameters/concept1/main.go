d1c989e11d5909ae427af95c6dcfab428e0f1306
oumouma <garveyshah1595@gmail.com>
2024-08-14 22:01:22 +0300

    function as return value

--- practice/funcs-as-parameters/concept1/main.go
+++ practice/funcs-as-parameters/concept1/main.go
@@ -3,9 +3,21 @@ package main
 import "fmt"
 
 func main() {
+	// Function as Parameters in Golang
 	Plate(chicken, "rice")
 	Plate(dal, "rice")
+
+	// Function as a return value in Golang
+
+	// het function as return value
+	fn := plate("rice")
+
+	// call the return value as the function
+	fn(" with chicken")
+	fn(" with fish")
 }
+
+// Function as Parameters in Golang
 
 func Plate(f func() string, primaryDish string) {
 	fmt.Println("This Plate Will Hve - " + primaryDish + f())
@@ -17,4 +29,13 @@ func chicken() string {
 
 func dal() string {
 	return " with Dal" // This Plate Will Have - rice with Dal
+}
+
+// Function as a return value in Golang
+
+func plate(primaryDish string) func(param string) {
+	// function is treated as a return value
+	return func(param string) {
+		fmt.Println("This plate Will have - " + primaryDish + param)
+	}
 }
