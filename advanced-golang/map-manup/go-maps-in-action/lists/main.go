9882ac2c4887b28a97d8ff0f50d98882e9b72b02
oumouma <garveyshah1595@gmail.com>
2024-08-30 22:47:45 +0300

    lists and maps

--- advanced-golang/map-manup/go-maps-in-action/lists/main.go
+++ advanced-golang/map-manup/go-maps-in-action/lists/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Node struct {
 	Next  *Node
 	Value interface{}
@@ -15,4 +17,21 @@ func main() {
 	node1.Next = node2
 	node2.Next = node3
 	// Uncomment the  following line to create a cycle
+	// node3.Next = node1
+
+	// Starting point of the linked list
+	var first *Node = node1
+
+	// Map to track visited nodes
+	visited := make(map[*Node]bool)
+
+	// Traverse the list and detect cycles
+	for n := first; n != nil; n = n.Next {
+		if visited[n] {
+			fmt.Println("Cycle detected")
+			break
+		}
+		visited[n] = true
+		fmt.Println(n.Value)
+	}
 }
