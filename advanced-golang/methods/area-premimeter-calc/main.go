cdf16b2162f4e7d9a38c693d7e0bca8362610a65
garveyshah <garveyshah1595@gmail.com>
2024-09-03 17:44:29 +0300

    argument reading

--- advanced-golang/methods/area-premimeter-calc/main.go
+++ advanced-golang/methods/area-premimeter-calc/main.go
@@ -1,39 +1,106 @@
 package main
 
 import (
-	"math"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
 
-type Circle struct {
-	radius float64
-}
+// type Circle struct {
+// 	radius float64
+// }
 
-type Rectangle struct {
-	length int
-	width  int
-}
+// type Rectangle struct {
+// 	length int
+// 	width  int
+// }
 
-type Triangle struct {
-	height int
-	base   int
-}
+// type Triangle struct {
+// 	hypoteneus int
+// 	height     int
+// 	base       int
+// }
 
-func (c Circle) Area() float64 {
-	return math.Pi * c.radius * c.radius
-}
+// // Area Methods
+// func (c Circle) Area() float64 {
+// 	return math.Pi * c.radius * c.radius
+// }
 
-func (t Triangle) Area() int {
-	return t.base / 2 * t.height
-}
+// func (t Triangle) Area() int {
+// 	return t.base / 2 * t.height
+// }
 
-func (r Rectangle) Area() int {
-	return r.length * r.width
-}
+// func (r Rectangle) Area() int {
+// 	return r.length * r.width
+// }
 
-func (c Circle) Perimeter() float64 {
-	return math.Pi * 2 * c.radius
-}
+// // Perimeter methods
 
-func (t Triangle) Perimeter() int {
-	return t.base + t.height
+// func (c Circle) Perimeter() float64 {
+// 	return math.Pi * 2 * c.radius
+// }
+
+// func (t Triangle) Perimeter() int {
+// 	return t.base + t.height + t.hypoteneus
+// }
+
+// func (r Rectangle) Perimeter() int {
+// 	return 2*r.length + 2*r.width
+// }
+
+// // Interfaces
+
+// type Perimeter interface {
+// 	Perimeter()
+// }
+
+// type Area interface {
+// 	Area()
+// }
+
+// func ()getData(shape target interface{}) interface{} {
+// 	var shape string
+// 	if len(os.args) < 2 {
+
+// 	}
+// 	switch len(os.Args) {
+// 		case len(os.Args) < 1 && len(os.Args) >
+// 	}
+// }
+
+// takes poiner to struct
+
+func main() {
+	fmt.Println("Enter shape and formal\nExample : area circle")
+	reader := bufio.NewReader(os.Stdin)
+	request, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error ", err)
+	}
+
+	shape := strings.Split(request, " ")[0]
+	formula := strings.Split(request, " ")[1]
+
+	 
+	//	shape = strings.ToLower(shape)
+	switch strings.ToLower(shape) {
+	case "rectangle":
+		fmt.Printf("Enter the dimentions of the %v in this order:  <length><space><width><height>\n5 6 7\n", shape)
+	case "triangle":
+		fmt.Printf("Enter the dimentions of the %v in this order:  <base><space><height><space><hypoteneuse>\n5 6 7\n", shape)
+	case "circle":
+		fmt.Printf("Enter the radius of the %v.\n", shape)
+	}
+
+	reader1 := bufio.NewReader(os.Stdin)
+	data, err := reader1.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error ", err)
+	}
+
+
+	fmt.Printf(formula, data, shape, "\n")
+
+	
 }
