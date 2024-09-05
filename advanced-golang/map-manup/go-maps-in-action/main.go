0e38197d70e625a2b6e4aacbfe9aa8371917fa07
oumouma <garveyshah1595@gmail.com>
2024-09-05 09:29:42 +0300

    chore

--- advanced-golang/map-manup/go-maps-in-action/main.go
+++ advanced-golang/map-manup/go-maps-in-action/main.go
@@ -48,43 +48,79 @@ func main() {
 	/*This example traverses a linked list of Nodes and prints their values.
 	It uses a map of Node pointers to detect cycles in the list.*/
 
-	type Node struct {
-		Next  *Node
-		Value interface{}
-	}
+	// type Node struct {
+	// 	Next  *Node
+	// 	Value interface{}
+	// }
 
-	var first *Node
+	// var first *Node
 
-	visited := make(map[*Node]bool)
-	for n := first; n != nil; n = n.Next {
-		if visited[n] {
-			fmt.Println("Cycle detected")
-			break
-		}
-		visited[n] = true
-		fmt.Println(n.Value)
-	}
+	// visited := make(map[*Node]bool)
+	// for n := first; n != nil; n = n.Next {
+	// 	if visited[n] {
+	// 		fmt.Println("Cycle detected")
+	// 		break
+	// 	}
+	// 	visited[n] = true
+	// 	fmt.Println(n.Value)
+	// }
 
-	/*In the following example, the slice people is populated with Person values. Each Person has a Name and a slice of Likes.
-	The example creates a map to associate each like with a slice of people that like it.*/
+	// /*In the following example, the slice people is populated with Person values. Each Person has a Name and a slice of Likes.
+	// The example creates a map to associate each like with a slice of people that like it.*/
 
-	type Person struct {
-		Name  string
-		Likes []string
-	}
+	// type Person struct {
+	// 	Name  string
+	// 	Likes []string
+	// }
 
-	var people []*Person
+	// var people []*Person
 
-	likes := make(map[string][]*Person)
-	for _, p := range people {
-		for _, l := range p.Likes {
-			likes[l] = append(likes[l], p)
-		}
-	}
+	// likes := make(map[string][]*Person)
+	// for _, p := range people {
+	// 	for _, l := range p.Likes {
+	// 		likes[l] = append(likes[l], p)
+	// 	}
+	// }
 
-	// To print a list of people who  who like cheese:
-	for _, p := range likes["cheese"] {
-		fmt.Println(p.Name, "likes cheese")
-		fmt.Println(len(likes["beacon"]), "people like bacon.")
-	}
+	// // To print a list of people who  who like cheese:
+	// for _, p := range likes["cheese"] {
+	// 	fmt.Println(p.Name, "likes cheese")
+	// }
+	// fmt.Println(len(likes["beacon"]), "people like bacon.")
+
+	// // Key Types
+	// // Maps keys maybe of any type that is comparable.
+	// // Comparable types are boolean, numeric, string, pointer, channel,
+	// // and interface types, and structs or arrays that contain only those types.
+	// // slices, maps and functions cannot be compared using == and my not be used as map keys.
+
+	// // Struct can be used to key data by multiple dimensions. For example,
+	// // this map of maps can be used to tally web page hits by country:
+	// hits := make(map[string]map[string]int)
+
+	// // Each key of thee outer map is the path to a web page with its own inner map. Each nner map key is a two-letter country code. This expression retrives the numbber of times an Australian has loaded the documentation page:
+	// //n := hits["/doc/"]["au"]
+
+	// // This approach becomes unwieldly when adding data, as for any given outer key you must check if the inner map exists,
+	// // and create it if needed:
+
+	// add(hits, "/doc/", "au")
+	// add(hits, "/doc/", "au")
+	// fmt.Println(hits)
+	// fmt.Println(hits["/doc/"])
+	// fmt.Println(hits["/doc/"]["au"])
+
+		// however the dsign that uses a single map with a does away all that commplexity
+		
+	
 }
+
+func add(m map[string]map[string]int, path, country string) {
+	mm, ok := m[path]
+	if !ok {
+		mm = make(map[string]int)
+		m[path] = mm
+	}
+	mm[country]++
+}
+
