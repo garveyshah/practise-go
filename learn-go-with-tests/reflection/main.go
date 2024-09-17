e9a3896529453aa0650df4d9837f25146ddbca62
oumouma <garveyshah1595@gmail.com>
2024-09-18 01:54:30 +0300

    reflection in go

--- learn-go-with-tests/reflection/main.go
+++ learn-go-with-tests/reflection/main.go
@@ -1,5 +1,9 @@
 package main
 
+import "reflect"
+
 func Walk(x interface{}, fn func(string)) {
-	fn("I still can't believe South Korea beat Germany 2 -0 to put them last in their group")
+	val := reflect.ValueOf(x)
+	field := val.Field(0)
+	fn(field.String())
 }
