8e3b27a4c73c8407e6072f52136741850b79a21d
oumouma <garveyshah1595@gmail.com>
2024-09-17 05:32:00 +0300

    Update: add string to pass test

--- learn-go-with-tests/reflection/main.go
+++ learn-go-with-tests/reflection/main.go
@@ -1,3 +1,5 @@
 package main
 
-func Walk(x interface{}, fn func(string)) {}
+func Walk(x interface{}, fn func(string)) {
+	fn("I still can't believe South Korea beat Germany 2 -0 to put them last in their group")
+}
