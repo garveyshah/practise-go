efe4918284627df739f89673b267ea1c21f4cee6
oumouma <garveyshah1595@gmail.com>
2024-09-28 10:31:33 +0300

    feat: final implementation

--- checkpoint-practise/fifthandskip/main.go
+++ checkpoint-practise/fifthandskip/main.go
@@ -1,8 +1,48 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+)
 
 func main() {
-
-fmt.Println("Hellow World")
+	fmt.Println(FifthAndSkip("e 5£ @ 8* 7 =56 ;"))
 }
 
+func FifthAndSkip(str string) string {
+	if str == "" {
+		return "\n"
+	}
+
+	if len(str) < 5 {
+		return "Invalid Input\n"
+	}
+
+	str1 := Clean(str)
+
+	clean := ""
+	count := 0
+
+	for _, char := range str1 {
+		if count == 5 {
+			clean += string(' ')
+			count = 0
+		} else {
+			clean += string(char)
+			count++
+
+		}
+	}
+	return clean + "\n"
+}
+
+func Clean(s string) (res string) {
+	for _, char := range s {
+		if char == ' ' {
+			continue
+		} else {
+			res += string(char)
+		}
+	}
+	// fmt.Println(res)
+	return res
+}
