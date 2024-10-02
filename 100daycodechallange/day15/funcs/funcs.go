824a2dfbacd28a6cb0e2805e00f024de835234b0
oumouma <garveyshah1595@gmail.com>
2024-10-02 21:41:43 +0300

    feat: inial function structure

---
+++ 100daycodechallange/day15/funcs/funcs.go
@@ -0,0 +1,11 @@
+package funcs
+
+// Atoi() converts a string of digits to interger
+func Atoi(yearS string) (yearN int, err error) {
+	return yearN, nil
+}
+
+// IsLeapYear() checks if a year is a leap year or not
+func IsLeapYear(year int) (is bool) {
+	return is
+}
