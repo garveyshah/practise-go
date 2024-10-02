d08e946003f6da02c0ff3c5b665b1a03dfa23bd6
oumouma <garveyshah1595@gmail.com>
2024-10-02 22:21:43 +0300

    feat: implementation of Atoi()

--- 100daycodechallange/day15/funcs/funcs.go
+++ 100daycodechallange/day15/funcs/funcs.go
@@ -1,7 +1,30 @@
 package funcs
 
+import "fmt"
+
 // Atoi() converts a string of digits to interger
 func Atoi(yearS string) (yearN int, err error) {
+	if yearS == "0" {
+		return 0, nil
+	}
+
+	var sign bool
+	if yearS[0] == '-' {
+		sign = true
+		yearS = yearS[1:]
+	}
+
+	for _, char := range yearS {
+		if char < '0' || char > '9' {
+			return 0, fmt.Errorf("invalid character %q", string(char))
+		} else {
+			yearN = yearN*10 + int(char-'0')
+		}
+	}
+
+	if sign {
+		yearN *= -1
+	}
 	return yearN, nil
 }
 
