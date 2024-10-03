8a74dc973b753362e56f7e26fa21834a4e35f0cf
oumouma <garveyshah1595@gmail.com>
2024-10-03 23:09:25 +0300

    refactor: rewrote the reader logic as functions

--- 100daycodechallange/day16/main.go
+++ 100daycodechallange/day16/main.go
@@ -15,9 +15,9 @@ import (
 
 func main() {
 	fmt.Println("Hello welcome to Ouma's sorted array merger CLI>")
-	fmt.Println("There's two ways to feed in data :\n")
+	fmt.Printf("There's two ways to feed in data :\n\n")
 	fmt.Println("	1. Data in a txt file")
-	fmt.Println("	2. Type in input from the terminal\n")
+	fmt.Printf("	2. Type in input from the terminal\n\n")
 	fmt.Println("Enter Option")
 
 	reader := bufio.NewReader(os.Stdin)
@@ -46,46 +46,26 @@ func main() {
 		data2 = data1
 		fmt.Println(data2)
 	} else if num == 2 {
-		fmt.Println("Type in slice1 - data : ... \n")
-		reader := bufio.NewReader(os.Stdin)
-		data, err := reader.ReadString('\n')
+		fmt.Printf("Type in slice1 - data : ... \n\n")
+
+		var reader funcs.SliceReader
+		var slice funcs.InputSlice
+
+		reader = slice
+
+		slice1, err := reader.Reader()
 		if err != nil {
-			fmt.Println("Error: ", err)
+			fmt.Println("Err :", err)
 			return
 		}
 
-		data = strings.TrimSpace(data)
-		slice := strings.Fields(data)
-		var slice1 []int
+		fmt.Printf("Type in slice2 - data : ... \n\n")
 
-		for _, digit := range slice {
-			num, err := strconv.Atoi(digit)
-			if err != nil {
-				fmt.Println("Err: ", err)
-				return
-			}
-			slice1 = append(slice1, num)
-		}
-
-		fmt.Println("Type in slice2 - data : ... \n")
-		reader2 := bufio.NewReader(os.Stdin)
-		data2, err := reader2.ReadString('\n')
+		reader = slice
+		slice2, err := reader.Reader()
 		if err != nil {
-			fmt.Println("Error: ", err)
+			fmt.Println("Err : ", err)
 			return
-		}
-
-		data2 = strings.TrimSpace(data2)
-		slice0 := strings.Fields(data2)
-		var slice2 []int
-
-		for _, digit := range slice0 {
-			num, err := strconv.Atoi(digit)
-			if err != nil {
-				fmt.Println("Err: ", err)
-				return
-			}
-			slice2 = append(slice2, num)
 		}
 
 		fmt.Printf("Slice 1 :- %v,\n\nSlice 2 :- %v\n\n", slice1, slice2)
