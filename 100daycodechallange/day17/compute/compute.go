9435019ad528207e6553de99c6dcac8a13d8f2a9
oumouma <garveyshah1595@gmail.com>
2024-10-04 22:37:09 +0300

    feat: function to calculate the nth fibonnaci number

--- 100daycodechallange/day17/compute/compute.go
+++ 100daycodechallange/day17/compute/compute.go
@@ -1,21 +1,9 @@
 package compute
 
-func Fibonnaci(n int) (fib int) {
-	if n < 0 {
-		return 0
+func Fibonnaci(n int) int {
+	if n <= 1 {
+		return n
 	}
 
-	// if n == 1 {
-	// 	return 1
-	// }
-
-	var a, b, d int = 0, 1, 0
-
-	for i := 0; i <= n; i++ {
-		c := a + b 
-		b = c
-		a = b
-		d = c
-	}
-	return d
+	return Fibonnaci(n-1) + Fibonnaci(n-2)
 }
