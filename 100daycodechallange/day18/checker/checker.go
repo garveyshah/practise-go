ab46232b757337ddc21a8a1fc0f268910c6616c6
oumouma <garveyshah1595@gmail.com>
2024-10-08 01:44:12 +0300

    implementation of the functions

---
+++ 100daycodechallange/day18/checker/checker.go
@@ -0,0 +1,25 @@
+package checker
+
+func Checker(s []string) string {
+	shortest := sort(s)
+
+	for i := 0; i < len(shortest); i++ {
+		for _, word := range s {
+			if shortest[i] != word[i] {
+				return shortest[:i]
+			}
+		}
+	}
+
+	return shortest
+}
+
+func sort(s []string) string {
+	shortest := s[0]
+	for _, word := range s {
+		if len(word) < len(shortest) {
+			shortest = word
+		}
+	}
+	return shortest
+}
