c22508d5a45c93ed23c5e8b379178d9973866a5c
oumouma <garveyshah1595@gmail.com>
2024-10-08 01:44:34 +0300

    testing

---
+++ 100daycodechallange/day18/checker/checker_test.go
@@ -0,0 +1,50 @@
+package checker
+
+import (
+	"testing"
+)
+
+func TestChecker(t *testing.T) {
+	tt := []struct {
+		input []string
+		want  string
+	}{
+		{[]string{"interview", "intermediate", "internal", "internet"}, "inter"},
+		{[]string{"apple", "apricot", "app", "append"}, "ap"},
+		{[]string{"station", "stable", "stand", "stair"}, "sta"},
+		{[]string{"flight", "fling", "flip", "float"}, "fl"},
+		{[]string{"teacher", "team", "teapot", "teal"}, "tea"},
+		{[]string{"transport", "translate", "transmit", "transform"}, "trans"},
+		{[]string{"sunshine", "sunday", "sunflower", "sunburn"}, "sun"},
+	}
+
+	for _, tc := range tt {
+		got := Checker(tc.input)
+
+		if got != tc.want {
+			t.Errorf("For input %q - Failed -  got= %q, want= %q", tc.input, got, tc.want)
+		}
+	}
+}
+
+func TestSort(t *testing.T) {
+	tt := []struct {
+		input []string
+		want  string
+	}{
+		{[]string{"interview", "intermediate", "internal", "internet"}, "internal"},
+		{[]string{"apple", "apricot", "app", "append"}, "app"},
+		{[]string{"station", "stable", "standup", "stair"}, "stair"},
+		{[]string{"flight", "fling", "flip", "float"}, "flip"},
+		{[]string{"teacher", "teams", "teapot", "teal"}, "teal"},
+		{[]string{"transport", "translate", "transmit", "transform"}, "transmit"},
+		{[]string{"sunshine", "sunday", "sunflower", "sunburn"}, "sunday"},
+	}
+
+	for _, tc := range tt {
+		got := sort(tc.input)
+		if got != tc.want {
+			t.Errorf("For input %q - Failed -  got= %q, want= %q", tc.input, got, tc.want)
+		}
+	}
+}
