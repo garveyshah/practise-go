3dd45d0514c0f1ee92d25b2639451f4b8f2f69fd
oumouma <garveyshah1595@gmail.com>
2024-10-09 02:47:41 +0300

    primes

--- 100daycodechallange/day19/calc/math.go
+++ 100daycodechallange/day19/calc/math.go
@@ -4,8 +4,20 @@ func Power(num, exp int) (power int) {
 	if exp == 0 {
 		return 1
 	}
-	for i := 1; i < exp; i++ {
+	power = 1
+	for i := 1; i <= exp; i++ {
 		power = power * num
 	}
-	return power * power
+	return power
 }
+
+func Factors(n int) []int {
+	factors := []int{}
+	for i := 1; i <= n; i++ {
+		if n%i == 0 {
+			factors = append(factors, i)
+		}
+	}
+	return factors
+}
+// funct primeFa
--- 100daycodechallange/day19/main.go
+++ 100daycodechallange/day19/main.go
@@ -2,30 +2,29 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"project01/100daycodechallange/day19/calc"
-	"strconv"
 )
 
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Println("Usage : go run . <number>\nExample : go run . 90")
-		return
-	}
+	// if len(os.Args) != 3 {
+	// 	fmt.Println("Usage : go run . <number>\nExample : go run . 90")
+	// 	return
+	// }
 
-	num, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		fmt.Println("Err : ", err)
-		return
-	}
+	// num, err := strconv.Atoi(os.Args[1])
+	// if err != nil {
+	// 	fmt.Println("Err : ", err)
+	// 	return
+	// }
 
-	exp, err := strconv.Atoi(os.Args[2])
-	if err != nil {
-		fmt.Println("Err : ", err)
-		return
-	}
+	// exp, err := strconv.Atoi(os.Args[2])
+	// if err != nil {
+	// 	fmt.Println("Err : ", err)
+	// 	return
+	// }
 
-	power := calc.Power(num, exp)
+	// power := calc.Power(num, exp)
 
-	fmt.Printf("The power of %d  raised to the number %d is %d", num, exp, power)
+	// fmt.Printf("%d raised to the power %d is \"%d\"\n", num, exp, power)
+	fmt.Println(calc.Factors(180))
 }
