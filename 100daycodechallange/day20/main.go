a17bbb9dc32ad477903a716d58dd21a12a5874c0
oumouma <garveyshah1595@gmail.com>
2024-10-18 06:09:46 +0300

    intersection

--- 100daycodechallange/day20/main.go
+++ 100daycodechallange/day20/main.go
@@ -7,25 +7,25 @@ import (
 func main() {
 	// nums1 := []int{1,2,2,1}
 	// nums2 := []int{2,2}
-	nums1 := []int{4,9,5}
-	 nums2 := []int{9,4,9,8,4}
+	nums1 := []int{4, 9, 5}
+	nums2 := []int{9, 4, 9, 8, 4}
 
-fmt.Println(intersection(nums1, nums2))
+	fmt.Println(intersection(nums1, nums2))
 }
 
 func intersection(nums1 []int, nums2 []int) []int {
-    myMap := make(map[int]struct{})
-    var result []int
+	myMap := make(map[int]struct{})
+	var result []int
 
-    for _, num := range nums1 {
-        myMap[num] = struct{}{}
-    }
+	for _, num := range nums1 {
+		myMap[num] = struct{}{}
+	}
 
-    for _, num := range nums2 {
-        if _, exists := myMap[num]; exists {
-            result = append(result, num)
-        delete(myMap, num)
-        }
-    }
-    return result    
+	for _, num := range nums2 {
+		if _, exists := myMap[num]; exists {
+			result = append(result, num)
+			delete(myMap, num)
+		}
+	}
+	return result
 }
