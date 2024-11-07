2092a9d520db4bffc0e108839c2980da65b4dfa5
oumouma <garveyshah1595@gmail.com>
2024-11-07 23:56:45 +0300

    [Practise] - repeatalpha

---
+++ checkpoint-practise/repeatalpha/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func RepeatAlpha(str string) string {
+	res := ""
+	count := 0
+
+	for _, char := range str {
+		if char >= 'A' && char <= 'Z' {
+			count = int(char - 'A' + 1)
+		} else if char >= 'a' && char <= 'z' {
+			count = int(char - 'a' + 1)
+		} else {
+			count = 1
+		}
+
+		for i := 0; i < count; i++ {
+			res += string(char)
+		}
+	}
+	return res
+}
+
+func main() {
+	fmt.Println(RepeatAlpha("abc"))
+	fmt.Println(RepeatAlpha("Choumi."))
+	fmt.Println(RepeatAlpha(""))
+	fmt.Println(RepeatAlpha("abacadaba 01!"))
+
+	testCases := []struct {
+		in   string
+		want string
+	}{
+		{"abc", "abbccc"},
+		{"Choumi.", "CCChhhhhhhhooooooooooooooouuuuuuuuuuuuuuuuuuuuummmmmmmmmmmmmiiiiiiiii."},
+		{"", ""},
+		{"abacadaba 01!", "abbacccaddddabba 01!"},
+	}
+	for _, tc := range testCases {
+		got := RepeatAlpha(tc.in)
+		if got != tc.want {
+			fmt.Printf("RepeatAlpha(%q) = %q instead of %q\n", tc.in, got, tc.want)
+			os.Exit(1)
+		}
+	}
+}
