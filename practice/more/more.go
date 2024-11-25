5fd066be20512240340875533529b1dc7126a552
oumouma <garveyshah1595@gmail.com>
2024-11-25 13:37:45 +0300

    more on structs, methods, and interfaces

---
+++ practice/more/more.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("Enter ypur data (type 'exit' to quit):")
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.S
+}
