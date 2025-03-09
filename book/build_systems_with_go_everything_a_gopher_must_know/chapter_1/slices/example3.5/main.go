1d6190503f19c9de3413b07843f1e58a437842e8
garveyshah <garveyshah1595@gmail.com>
2025-03-09 18:00:02 +0300

    modified ouput

--- book/build_systems_with_go_everything_a_gopher_must_know/chapter_1/slices/example3.5/main.go
+++ book/build_systems_with_go_everything_a_gopher_must_know/chapter_1/slices/example3.5/main.go
@@ -1,6 +1,8 @@
 // 3.2.1 Length and capacity
 // The common operation on a slice is the iteration through its items.
 // Any 'loop' is a good candidate to do this
+// For arrays and slices, range returns the index of the item and the item itself for every iteration of the loop.
+// The iteration is always done incrementally from index 0 to index n-1
 
 package main
 
@@ -11,11 +13,14 @@ func main() {
 		"Jeremy", "John", "Joseph",
 	}
 
-	for i := 0; i < len(names); i++ {
-		fmt.Println(i, names[i])
-	}
+	// for i := 0; i < len(names); i++ {
+	// 	// fmt.Println(i, names[i])
+	// 	name = name + "_changed"
+	// }
 
 	for position, name := range names {
-		fmt.Println(position, name)
+		// fmt.Println(position, name)
+		names[position] = name + "_changed"
 	}
+	fmt.Println(names)
 }
